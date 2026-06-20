f238d08b1a09da395fd0541914da5ac5076e6e21
= <=>
2026-06-20 15:37:04 -0300

    Exercício Sexta

---
+++ Exercicio_3.js
@@ -0,0 +1,20 @@
+let nome1 = "Leo"
+let nome2 = "Joao"
+let nome3 = "Pedro"
+
+let ingresso1 = "Valido"
+let ingresso2 = "Invalido"
+let ingresso3 = "Invalido"
+
+let idade1 = 18
+let idade2 = 22
+let idade3 = 16
+
+if (idade1 >= 18 && ingresso1 == "Valido") {console.log("Válido")} 
+else {console.log("Inválido")}
+
+if (idade2 >= 18 && ingresso2 == "Valido") {console.log("Válido")} 
+else {console.log("Inválido")}
+
+if (idade3 >= 18 && ingresso3 == "Valido") {console.log("Válido")} 
+else {console.log("Inválido")}
---
+++ Exercicio_4.js
@@ -0,0 +1,5 @@
+let idade1 = 18
+let idade2 = 17 
+
+if (idade1 >= 18) {console.log("Pode Dirigir")}
+if (idade2 >= 18) {console.log("Pode Dirigir")} else {console.log("NÃO Pode Dirigir")}
