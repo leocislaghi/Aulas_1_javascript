86365db723bfed7adb8f91572e62d9c594c85a20
= <=>
2026-06-23 20:22:54 -0300

    Switch

---
+++ Exercicio_7.js
@@ -0,0 +1,24 @@
+let opcao = 3
+
+// 1 = Pizza
+// 2 = Hambúrguer
+// 3 = Salada
+// 4 = Refrigerante
+
+switch (opcao) {
+
+case 1: 
+    console.log("Pedido tradicional")
+    break;
+case 2: 
+    console.log("Pedido tradicional")
+    break;
+case 3: 
+    console.log("Pedido saudável")
+    break;
+case 4: 
+    console.log("Bebida escolhida")
+    break;
+default: 
+    console.log("Alimento escolhido")
+}
